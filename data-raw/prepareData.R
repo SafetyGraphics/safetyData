c1b90494cbc8cddc5469001d7d14372e0d26d3af
jwildfire <jwildfire@gmail.com>
2021-01-08 13:11:18 -0500

    incorporate metadata in roxygen documentation

--- data-raw/prepareData.R
+++ data-raw/prepareData.R
@@ -2,6 +2,8 @@
 library(dplyr)
 library(usethis)
 
+# Clear global environment
+rm(list = ls())
 
 # Copy csv files in /data-raw to /data and create roxygen documentation in /R
 paths<-list.files(
@@ -13,32 +15,43 @@ paths<-list.files(
 )  
 
 paths <- paths[!grepl("meta",paths, fixed=TRUE)]
-#metaCols <- read.csv('data-raw/meta_var.csv')
-#metaDomains <- read.csv('data-raw/meta_study.csv')
+
 fileList<-list()
 for(path in paths){
     file <- substr(path,1,nchar(path)-4) # remove file extension
     assign(file, read.csv(paste0('data-raw/',path), stringsAsFactors = FALSE,  na.strings = c("NA",""))) # Read csv to data frame
     do.call('use_data', list(as.name(file), overwrite = TRUE)) #save to .RDA to /data 
-    
-    # metaCol <- metaCols %>% filter(Form==file)
-    # metaStudy <- metaStudy %>% filter(Dataset==file)
 }
 
 # Generate basic roxygen documentation
 # Thanks to: https://stackoverflow.com/questions/51092509/whats-the-best-way-to-automatically-generate-roxygen2-documentation-for-a-data
 
-df.names<-names(which(unlist(eapply(.GlobalEnv,is.data.frame))))
-dfs <- lapply(df.names,get)
-
+metaCols <- read.csv('data-raw/meta_var.csv')
+metaDomains <- read.csv('data-raw/meta_study.csv')
+#options(useFancyQuotes = FALSE)
 roxyheaders <- unlist(
     sapply(
-        df.names, 
-        function(d){
+        paths, 
+        function(path){
+            d<- substr(path,1,nchar(path)-4) # remove file extension
+            df<-get(d)
+            metaCol <- metaCols %>% filter(tolower(Form)==tolower(d))
+            metaDomain <- metaDomains %>% filter(tolower(Dataset)==tolower(d))
+            columns<-paste(
+                "#' \\item{\\code{",
+                metaCol$Variable,
+                "}}{",
+                metaCol$Type,
+                "-",
+                metaCol$Label,"}"
+            )
             roxy<-c(
-                paste("#'", d), 
-                "#' @format data.frame",
-                gsub("^","#'",capture.output(str(get(d)))),
+                paste("#' @title", d), 
+                paste("#' @description", metaDomain$Description," data frame -",metaDomain$Structure), 
+                paste("#' @format a data frame with",dim(df)[1], "rows and ", dim(df)[2]," columns:"),
+                paste("#' \\describe{"),
+                columns,
+                paste("#'}"),
                 dQuote(d),
                 "\n"
             )
@@ -48,6 +61,7 @@ roxyheaders <- unlist(
     ),
     use.names=FALSE
 )
+
 
 cat(roxyheaders, file="R/domains.R", sep="\n")
 
