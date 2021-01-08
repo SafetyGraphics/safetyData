aa9fc0af8a19df785de7abfa6d5ba3dc2cf8a1f9
jwildfire <jwildfire@gmail.com>
2021-01-08 14:48:49 -0500

    add sdtm prefix

--- R/domains.R
+++ R/domains.R
@@ -1,4 +1,4 @@
-#' @title ae
+#' @title sdtm_ae
 #' @description Adverse Events  data frame - One record per adverse event per subject
 #' @format a data frame with 1191 rows and  35  columns:
 #' \describe{
@@ -38,10 +38,10 @@
 #' \item{\code{ AESTDY }}{ integer - Study Day of Start of Adverse Event }
 #' \item{\code{ AEENDY }}{ integer - Study Day of End of Adverse Event }
 #'}
-"ae"
+"sdtm_ae"
 
 
-#' @title cm
+#' @title sdtm_cm
 #' @description Concomitant Medications  data frame - One record per recorded medication occurrence or constant-dosing interval per subject
 #' @format a data frame with 7510 rows and  21  columns:
 #' \describe{
@@ -67,10 +67,10 @@
 #' \item{\code{ CMSTDY }}{ integer - Study Day of Start of Medication }
 #' \item{\code{ CMENDY }}{ integer - Study Day of End of Medication }
 #'}
-"cm"
+"sdtm_cm"
 
 
-#' @title dm
+#' @title sdtm_dm
 #' @description Demographics  data frame - One record per subject
 #' @format a data frame with 306 rows and  25  columns:
 #' \describe{
@@ -100,10 +100,10 @@
 #' \item{\code{ DMDTC }}{ date - Date/Time of Collection }
 #' \item{\code{ DMDY }}{ integer - Study Day of Collection }
 #'}
-"dm"
+"sdtm_dm"
 
 
-#' @title ds
+#' @title sdtm_ds
 #' @description Disposition  data frame - One record per disposition status or protocol milestone per subject
 #' @format a data frame with 596 rows and  13  columns:
 #' \describe{
@@ -121,10 +121,10 @@
 #' \item{\code{ DSSTDTC }}{ date - Start Date/Time of Disposition Event }
 #' \item{\code{ DSSTDY }}{ integer - Study Day of Start of Disposition Event }
 #'}
-"ds"
+"sdtm_ds"
 
 
-#' @title ex
+#' @title sdtm_ex
 #' @description Exposure  data frame - One record per constant dosing interval per subject
 #' @format a data frame with 591 rows and  17  columns:
 #' \describe{
@@ -146,10 +146,10 @@
 #' \item{\code{ EXSTDY }}{ integer - Study Day of Start of Treatment }
 #' \item{\code{ EXENDY }}{ integer - Study Day of End of Treatment }
 #'}
-"ex"
+"sdtm_ex"
 
 
-#' @title lb
+#' @title sdtm_lb
 #' @description Laboratory Tests Results  data frame - One record per analyte per planned time point number per time point reference per visit per subject
 #' @format a data frame with 59580 rows and  23  columns:
 #' \describe{
@@ -177,10 +177,10 @@
 #' \item{\code{ LBDTC }}{ datetime - Date/Time of Specimen Collection }
 #' \item{\code{ LBDY }}{ integer - Study Day of Specimen Collection }
 #'}
-"lb"
+"sdtm_lb"
 
 
-#' @title mh
+#' @title sdtm_mh
 #' @description Medical History  data frame - One record per medical history event per subject
 #' @format a data frame with 1818 rows and  19  columns:
 #' \describe{
@@ -204,10 +204,10 @@
 #' \item{\code{ MHSTDTC }}{ date - Start Date/Time of Medical History Event }
 #' \item{\code{ MHDY }}{ integer - Study Day of History Collection }
 #'}
-"mh"
+"sdtm_mh"
 
 
-#' @title qs
+#' @title sdtm_qs
 #' @description Questionnaires  data frame - One record per questionnaire per question per time point per visit per subject
 #' @format a data frame with 121749 rows and  20  columns:
 #' \describe{
@@ -232,10 +232,10 @@
 #' \item{\code{ QSDTC }}{ date - Date/Time of Finding }
 #' \item{\code{ QSDY }}{ integer - Study Day of Finding }
 #'}
-"qs"
+"sdtm_qs"
 
 
-#' @title relrec
+#' @title sdtm_relrec
 #' @description Related Records  data frame - One record per related record, group of records or datasets
 #' @format a data frame with 234 rows and  7  columns:
 #' \describe{
@@ -247,10 +247,10 @@
 #' \item{\code{ RELTYPE }}{ text - Relationship Type }
 #' \item{\code{ RELID }}{ text - Relationship Identifier }
 #'}
-"relrec"
+"sdtm_relrec"
 
 
-#' @title sc
+#' @title sdtm_sc
 #' @description Subject Characteristics  data frame - One record per characteristic per subject
 #' @format a data frame with 254 rows and  14  columns:
 #' \describe{
@@ -269,10 +269,10 @@
 #' \item{\code{ SCDTC }}{ date - Date/Time of Collection }
 #' \item{\code{ SCDY }}{ integer - Study Day of Examination }
 #'}
-"sc"
+"sdtm_sc"
 
 
-#' @title se
+#' @title sdtm_se
 #' @description Subject Elements  data frame - One record per actual Element per subject
 #' @format a data frame with 752 rows and  9  columns:
 #' \describe{
@@ -286,10 +286,10 @@
 #' \item{\code{ SEENDTC }}{ date - End Date/Time of Element }
 #' \item{\code{ SEUPDES }}{ text - Description of Unplanned Element }
 #'}
-"se"
+"sdtm_se"
 
 
-#' @title suppae
+#' @title sdtm_suppae
 #' @description Supplemental Qualifiers for AE  data frame - One record per IDVAR, IDVARVAL, and QNAM value per subject
 #' @format a data frame with 1191 rows and  10  columns:
 #' \describe{
@@ -304,10 +304,10 @@
 #' \item{\code{ QORIG }}{ text - Origin }
 #' \item{\code{ QEVAL }}{ text - Evaluator }
 #'}
-"suppae"
+"sdtm_suppae"
 
 
-#' @title suppdm
+#' @title sdtm_suppdm
 #' @description Supplemental Qualifiers for DM  data frame - One record per IDVAR, IDVARVAL, and QNAM value per subject
 #' @format a data frame with 1197 rows and  10  columns:
 #' \describe{
@@ -322,10 +322,10 @@
 #' \item{\code{ QORIG }}{ text - Origin }
 #' \item{\code{ QEVAL }}{ text - Evaluator }
 #'}
-"suppdm"
+"sdtm_suppdm"
 
 
-#' @title suppds
+#' @title sdtm_suppds
 #' @description Supplemental Qualifiers for DS  data frame - One record per IDVAR, IDVARVAL, and QNAM value per subject
 #' @format a data frame with 3 rows and  10  columns:
 #' \describe{
@@ -340,10 +340,10 @@
 #' \item{\code{ QORIG }}{ text - Origin }
 #' \item{\code{ QEVAL }}{ text - Evaluator }
 #'}
-"suppds"
+"sdtm_suppds"
 
 
-#' @title supplb
+#' @title sdtm_supplb
 #' @description Supplemental Qualifiers for LB  data frame - One record per IDVAR, IDVARVAL, and QNAM value per subject
 #' @format a data frame with 64403 rows and  10  columns:
 #' \describe{
@@ -358,10 +358,10 @@
 #' \item{\code{ QORIG }}{ text - Origin }
 #' \item{\code{ QEVAL }}{ text - Evaluator }
 #'}
-"supplb"
+"sdtm_supplb"
 
 
-#' @title sv
+#' @title sdtm_sv
 #' @description Subject Visits  data frame - One record per actual visit per subject
 #' @format a data frame with 3559 rows and  8  columns:
 #' \describe{
@@ -374,10 +374,10 @@
 #' \item{\code{ SVSTDTC }}{ date - Start Date/Time of Visit }
 #' \item{\code{ SVENDTC }}{ date - End Date/Time of Visit }
 #'}
-"sv"
+"sdtm_sv"
 
 
-#' @title ta
+#' @title sdtm_ta
 #' @description Trial Arms  data frame - One record per planned Element per Arm
 #' @format a data frame with 8 rows and  10  columns:
 #' \describe{
@@ -392,10 +392,10 @@
 #' \item{\code{ TATRANS }}{ text - Transition Rule }
 #' \item{\code{ EPOCH }}{ text - Epoch }
 #'}
-"ta"
+"sdtm_ta"
 
 
-#' @title te
+#' @title sdtm_te
 #' @description Trial Elements  data frame - One record per planned Element
 #' @format a data frame with 7 rows and  7  columns:
 #' \describe{
@@ -407,10 +407,10 @@
 #' \item{\code{ TEENRL }}{ text - Rule for End of Element }
 #' \item{\code{ TEDUR }}{ text - Planned Duration of Element }
 #'}
-"te"
+"sdtm_te"
 
 
-#' @title ti
+#' @title sdtm_ti
 #' @description Trial Inclusion/ Exclusion Criteria  data frame - One record per I/E criterion
 #' @format a data frame with 31 rows and  6  columns:
 #' \describe{
@@ -421,10 +421,10 @@
 #' \item{\code{ IECAT }}{ text - Inclusion/Exclusion Category }
 #' \item{\code{ TIRL }}{ text - Inclusion/Exclusion Criterion Rule }
 #'}
-"ti"
+"sdtm_ti"
 
 
-#' @title ts
+#' @title sdtm_ts
 #' @description Trial Summary  data frame - One record per trial summary parameter value
 #' @format a data frame with 33 rows and  6  columns:
 #' \describe{
@@ -435,10 +435,10 @@
 #' \item{\code{ TSPARM }}{ text - Trial Summary Parameter }
 #' \item{\code{ TSVAL }}{ text - Parameter Value }
 #'}
-"ts"
+"sdtm_ts"
 
 
-#' @title tv
+#' @title sdtm_tv
 #' @description Trial Visits  data frame - One record per planned Visit per Arm
 #' @format a data frame with 21 rows and  9  columns:
 #' \describe{
@@ -452,10 +452,10 @@
 #' \item{\code{ TVSTRL }}{ text - Visit Start Rule }
 #' \item{\code{ TVENRL }}{ text - Visit End Rule }
 #'}
-"tv"
+"sdtm_tv"
 
 
-#' @title vs
+#' @title sdtm_vs
 #' @description Vital Signs  data frame - One record per vital sign measurement per time point per visit per subject
 #' @format a data frame with 29643 rows and  24  columns:
 #' \describe{
@@ -484,6 +484,6 @@
 #' \item{\code{ VSELTM }}{ text - Planned Elapsed Time from Time Point Ref }
 #' \item{\code{ VSTPTREF }}{ text - Time Point Reference }
 #'}
-"vs"
+"sdtm_vs"
 
 
--- data-raw/prepareData.R
+++ data-raw/prepareData.R
@@ -2,12 +2,9 @@ library(dplyr)
 library(usethis)
 library(here)
 
-# Clear global environment
-rm(list = ls())
-
 # Copy csv files in /data-raw to /data and create roxygen documentation in /R
 paths<-list.files(
-    here::here("data-raw"), 
+    here::here("data-raw","sdtm"), 
     pattern = "\\.csv$", 
     ignore.case=TRUE, 
     full.names=FALSE, 
@@ -16,53 +13,22 @@ paths<-list.files(
 
 paths <- paths[!grepl("meta",paths, fixed=TRUE)]
 
-fileList<-list()
 for(path in paths){
-    file <- substr(path,1,nchar(path)-4) # remove file extension
-    assign(file, read.csv(paste0('data-raw/',path), stringsAsFactors = FALSE,  na.strings = c("NA",""))) # Read csv to data frame
-    do.call('use_data', list(as.name(file), overwrite = TRUE)) #save to .RDA to /data 
+    # remove file extension
+    file <- paste0(
+        "sdtm_",
+        substr(path,1,nchar(path)-4)
+    ) 
+
+    # Read csv to data frame   
+    assign(
+        file, 
+        read.csv(here('data-raw','sdtm',path), stringsAsFactors = FALSE,  na.strings = c("NA",""))
+    ) 
+    
+    #save to .RDA to /data 
+    do.call(
+        'use_data', 
+        list(as.name(file), overwrite = TRUE)
+    ) 
 }
-
-# Generate basic roxygen documentation
-# Thanks to: https://stackoverflow.com/questions/51092509/whats-the-best-way-to-automatically-generate-roxygen2-documentation-for-a-data
-
-metaCols <- read.csv('data-raw/meta_var.csv')
-metaDomains <- read.csv('data-raw/meta_study.csv')
-#options(useFancyQuotes = FALSE)
-roxyheaders <- unlist(
-    sapply(
-        paths, 
-        function(path){
-            d<- substr(path,1,nchar(path)-4) # remove file extension
-            df<-get(d)
-            metaCol <- metaCols %>% filter(tolower(Form)==tolower(d))
-            metaDomain <- metaDomains %>% filter(tolower(Dataset)==tolower(d))
-            columns<-paste(
-                "#' \\item{\\code{",
-                metaCol$Variable,
-                "}}{",
-                metaCol$Type,
-                "-",
-                metaCol$Label,"}"
-            )
-            roxy<-c(
-                paste("#' @title", d), 
-                paste("#' @description", metaDomain$Description," data frame -",metaDomain$Structure), 
-                paste("#' @format a data frame with",dim(df)[1], "rows and ", dim(df)[2]," columns:"),
-                paste("#' \\describe{"),
-                columns,
-                paste("#'}"),
-                dQuote(d),
-                "\n"
-            )
-            return(roxy)
-        },
-        simplify=FALSE
-    ),
-    use.names=FALSE
-)
-
-
-cat(roxyheaders, file=here::here("R/domains.R"), sep="\n")
-
-
